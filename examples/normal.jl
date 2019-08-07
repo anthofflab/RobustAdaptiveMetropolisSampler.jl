b82f27da77c908c3a797fe7a51e19b8909dc0616
David Anthoff <anthoff@berkeley.edu>
2019-08-07 14:30:32 -0700

    Simplify example further

--- examples/normal.jl
+++ examples/normal.jl
@@ -1,8 +1,11 @@
 using RobustAdaptiveMetropolisSampler, Distributions, LinearAlgebra, VegaLite, DataFrames
 
-logp(p) = logpdf(Normal(3., 2), p[1])
-
-chain, accrate, S = RAM_sample(logp, [0.], 0.5, 100_000, q = TDist(1))
+chain, accrate, S = RAM_sample(
+    p -> logpdf(Normal(3., 2), p[1]), # log target function
+    [0.],                             # Initial values
+    0.5,                              # Scaling factor
+    100_000                           # Number of runs
+)
 
 df = DataFrame(p1 = chain[:,1])
 
