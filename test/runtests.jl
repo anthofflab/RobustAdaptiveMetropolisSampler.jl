2a2be4bc9d7de75ccb7822bf4307c39fd8b1de79
David Anthoff <anthoff@berkeley.edu>
2019-08-07 13:47:42 -0700

    Add tests for all methods

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using RobustAdaptiveMetropolisSampler
 using Distributions
 using Statistics
+using LinearAlgebra
 using Test
 
 @testset "RobustAdaptiveMetropolisSampler" begin
@@ -11,5 +12,15 @@ result = RAM_sample(p->logpdf(Normal(3.,2.), p[1]), [0.], 0.5, 100_000, show_pro
 @test result.acceptance_rate ≈ 0.234 atol=0.01
 @test mean(result.chain[:,1]) ≈ 3. atol=0.1
 @test_broken std(result.chain[:,1]) ≈ 2.
+
+result = RAM_sample(p->logpdf(Normal(3.,2.), p[1]), [0.], [0.5], 1_000, show_progress=false)
+@test length(result.chain) == 1_000
+
+result = RAM_sample(p->logpdf(Normal(3.,2.), p[1]), [0.], Diagonal([0.5]), 1_000, show_progress=false)
+@test length(result.chain) == 1_000
+
+
+result = RAM_sample(p->logpdf(Normal(3.,2.), p[1]), [0.], fill(0.5, 1, 1), 1_000, show_progress=false)
+@test length(result.chain) == 1_000
 
 end
