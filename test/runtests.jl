4b304a1194adf183a979df272f2859a436a5e5e1
David Anthoff <anthoff@berkeley.edu>
2019-09-30 17:32:33 -0700

    Fix a bug that a value wasn't updated if a proposal was accepted

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,7 @@ result = RAM_sample(p->logpdf(Normal(3.,2.), p[1]), [0.], 0.5, 100_000, show_pro
 @test length(result.chain) == 100_000
 @test result.acceptance_rate ≈ 0.234 atol=0.01
 @test mean(result.chain[:,1]) ≈ 3. atol=0.1
-@test_broken std(result.chain[:,1]) ≈ 2.
+@test std(result.chain[:,1]) ≈ 2. atol=0.01
 
 result = RAM_sample(p->logpdf(Normal(3.,2.), p[1]), [0.], [0.5], 1_000, show_progress=false)
 @test length(result.chain) == 1_000
